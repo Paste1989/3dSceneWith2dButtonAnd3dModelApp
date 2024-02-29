543ffe9e27cb9add633fed7614d5628f838a728d
sbrezo <brezovac.sasa@gmail.com>
2024-02-29 10:28:43 +0100

    Initial Commit

---
+++ 3dSceneApp/_dSceneAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  _dSceneAppApp.swift
+//  3dSceneApp
+//
+//  Created by Saša Brezovac on 29.02.2024..
+//
+
+import SwiftUI
+
+@main
+struct _dSceneAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
